bb709e7d7f5b7faa4185821e10c749766101587d
Mustafa Muhammad <m.muhammad43081@gmail.com>
2019-11-26 18:34:29 -0500

    connection issue resolved

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,9 @@
 -- SEED DATA FOR BURGERS_DB DATABASE
-INSERT INTO burgers (burger_name, devoured) VALUES ("Jive Turkey Cheeseburger", false);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Passyunk Portobello Burger", false);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Vegan Veggie Burger", false);
-INSERT INTO burgers (burger_name, devoured) VALUES ("South West Bean Burger", true);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Ol' Dirty Bastard Cheeseburger", true);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Bad'n'Bougie Burger on Brioche Bun", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Rza's Razor Sharp Cheddar Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Gza Genius Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("M.E.T.H.O.D Hungry Man Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Shaolin Shadowboxing Turkey Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("GhostFace Killa Bean Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Ol' Dirty Bastard Hamburger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Raekwon's Da Chefs Special Deer Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Brooklyn Zoo Bison Burger", true);
