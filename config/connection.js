644278d2def8790dcc2fef0e2dadac7ef377d83b
Mustafa Muhammad <m.muhammad43081@gmail.com>
2019-11-25 23:19:00 -0500

    error in code possible typo

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
-    conneciton = mysql.createConnection(process.env.JAWSDB_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
         host: "localhost",
@@ -14,14 +14,14 @@ if (process.env.JAWSDB_URL) {
     });
 }
 
-    // Make connection.
-    connection.connect(function(err) {
-        if (err) {
-            console.error("error connecting: " + err.stack);
-            return;
-        }
-        console.log("connected as id " + connection.threadId);
-    });
+// Make connection.
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
 
-    // Export connection for our ORM to use.
-    module.exports = connection;
+// Export connection for our ORM to use.
+module.exports = connection;
