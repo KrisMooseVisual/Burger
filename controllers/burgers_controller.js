f475e19bd97eeef6194666b486aef1a3acc5dde4
Mustafa Muhammad <m.muhammad43081@gmail.com>
2019-11-27 15:58:24 -0500

    buttons displaying all over page and not just when clicked and added.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,11 +1,7 @@
 var express = require("express");
-
 var router = express.Router();
-
-// Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
 
-// Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
   burger.all(function(data) {
     var hbsObject = {
@@ -22,12 +18,11 @@ router.post("/api/burgers", function(req, res) {
   ], [
     req.body.name, req.body.devoured
   ], function(result) {
-    // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
+router.post("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
@@ -36,7 +31,6 @@ router.put("/api/burgers/:id", function(req, res) {
     devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
       res.status(200).end();
@@ -49,7 +43,6 @@ router.delete("/api/burgers/:id", function(req, res) {
 
   burger.delete(condition, function(result) {
     if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
       res.status(200).end();
@@ -57,5 +50,5 @@ router.delete("/api/burgers/:id", function(req, res) {
   });
 });
 
-// Export routes for server.js to use.
+// Export database
 module.exports = router;
