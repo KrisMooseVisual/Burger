4604d72fda8e1d2904786faab6091eed29054c16
Mustafa Muhammad <m.muhammad43081@gmail.com>
2019-11-28 10:17:27 -0500

    a bug has prevented me from connecting to server. Trying to find bug

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,8 +2,8 @@ var express = require("express");
 var router = express.Router();
 var burger = require("../models/burger.js");
 
-router.get("/", function(req, res) {
-  burger.all(function(data) {
+router.get("/", function (req, res) {
+  burger.all(function (data) {
     var hbsObject = {
       burgers: data
     };
@@ -12,24 +12,24 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/api/burgers", function(req, res) {
+router.post("/api/burgers", function (req, res) {
   burger.create([
     "burger_name", "devoured"
   ], [
     req.body.name, req.body.devoured
-  ], function(result) {
+  ], function (result) {
     res.json({ id: result.insertId });
   });
 });
 
-router.post("/api/burgers/:id", function(req, res) {
+router.put("/api/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
   burger.update({
     devoured: req.body.devoured
-  }, condition, function(result) {
+  }, condition, function (result) {
     if (result.changedRows == 0) {
       return res.status(404).end();
     } else {
@@ -38,10 +38,10 @@ router.post("/api/burgers/:id", function(req, res) {
   });
 });
 
-router.delete("/api/burgers/:id", function(req, res) {
+router.delete("/api/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
-  burger.delete(condition, function(result) {
+  burger.delete(condition, function (result) {
     if (result.affectedRows == 0) {
       return res.status(404).end();
     } else {
--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,49 +1,49 @@
-// $(function () {
+$(function () {
 
-//     $(".create-form").on("submit", function (event) {
-//         event.preventDefault();
+    $(".create-form").on("submit", function (event) {
+        event.preventDefault();
 
-//         var newBurger = {
-//             burger_name: $("#newburger")
-//                 .val().trim(),
-//             devoured: 0
-//         };
+        var newBurger = {
+            burger_name: $("#newburger")
+                .val().trim(),
+            devoured: 0
+        };
 
-//         $.ajax("api/burgers", {
-//             type: "POST",
-//             data: newBurger
-//         }).then(function () {
-//             console.log("Added Da New Burger");
-//             location.reload();
-//         });
+        $.ajax("api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(function () {
+            console.log("Added Da New Burger");
+            location.reload();
+        });
 
-//     });
+    });
 
-//     $(".devourBurger").on("click", function (event) {
-//         event.preventDevault();
+    $(".devourBurger").on("click", function (event) {
+        event.preventDevault();
 
-//         var id = $(this).data("id");
-//         var devouredState = {
-//             devoured: 1
-//         };
+        var id = $(this).data("id");
+        var devouredState = {
+            devoured: 1
+        };
 
-//         $.ajax("api/burgers/" + id, {
-//             type: "PUT",
-//             data: devouredState
-//         }).then(function () {
-//             console.log("Da Burger Devoured");
-//             location.reload();
-//         });
-//     });
+        $.ajax("api/burgers/" + id, {
+            type: "PUT",
+            data: devouredState
+        }).then(function () {
+            console.log("Da Burger Devoured");
+            location.reload();
+        });
+    });
 
-//     $("trashBurger").on("click", function (event) {
-//         event.preventDefault();
+    $("trashBurger").on("click", function (event) {
+        event.preventDefault();
 
-//         var id = $(this).data("id");
+        var id = $(this).data("id");
 
-//         $.ajax({
-//             type: "delete",
-//             url: "api/burgers/" + id
-//         }).then(locatin.reload());
-//     });
-// });
+        $.ajax({
+            type: "delete",
+            url: "api/burgers/" + id
+        }).then(locatin.reload());
+    });
+});
