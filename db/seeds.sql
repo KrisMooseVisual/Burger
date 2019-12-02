28a2ad72ca1b1cf3e0ae13928c3433c384d754f9
Mustafa Muhammad <m.muhammad43081@gmail.com>
2019-12-01 21:39:23 -0500

    still having deployed heroku error

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,10 @@
-USE burgers_db;
 INSERT INTO burgers (burger_name, devoured)
-VALUES ("Rza's Razor Sharp Cheddar Cheeseburger", FALSE),
- ("The Gza Genius Burger", FALSE),
- ("M.E.T.H.O.D Hungry Man Burger", FALSE),
- ("Shaolin Shadowboxing Turkey Burger", FALSE),
- ("GhostFace Killa Bean Burger", FALSE),
- ("Ol' Dirty Bastard Hamburger", FALSE),
- ("Raekwon's Da Chefs Special Deer Burger", FALSE),
- ("Brooklyn Zoo Bison Burger", FALSE)
+VALUES ("Rza's Razor Sharp Cheddar Cheeseburger", 0),
+ ("The Gza Genius Burger", 0),
+ ("M.E.T.H.O.D Hungry Man Burger", 0),
+ ("Shaolin Shadowboxing Turkey Burger", 0),
+ ("GhostFace Killa Bean Burger", 0),
+ ("Ol' Dirty Bastard Hamburger", 0),
+ ("Raekwon's Da Chefs Special Deer Burger", 0),
+ ("Brooklyn Zoo Bison Burger", 0)
 
